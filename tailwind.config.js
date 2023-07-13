49e092ebf2a20fe89997309355902c410bb10ef7
Md. Arshad <arshadchowdhury46@gmail.com>
2023-07-14 05:14:09 +0600

    done setting up colors and decided to go with flex layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    colors: {
+      'dark-sky': '#2F80ED',
+      'mixed-sky': '#2568C1',
+      'dark-gray': '#252C32',
+      'mid-gray': '#777777',
+      'light-gray': '#E5E9EB',
+      'light-white': '#F7F7F7',
+      'dark-white': '#F8FBFF',
+    },
+  },
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,8 +19,5 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [],
 }
